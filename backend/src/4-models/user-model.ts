2ba0ffca155132d90c8da8df9830414bbc13a132
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2023-12-14 18:38:16 +0200

    Added randomId function to user-model.ts file.

--- backend/src/4-models/user-model.ts
+++ backend/src/4-models/user-model.ts
@@ -3,35 +3,41 @@ import Role from "./role-model";
 
 class UserModel {
 
-    public userId: number;
+    public id: number;
     public firstName: string;
     public lastName: string;
-    public username: string;
+    public email: string;
     public password: string;
     public tal: string;
     public role: Role;
      
     // Copy - Constructor
     public constructor(user: UserModel) {
-        this.userId = user.userId;
+        this.id = this.randomId();
         this.firstName = user.firstName;
         this.lastName = user.lastName;
-        this.username = user.username;
+        this.email = user.email;
         this.password = user.password;
+        this.tal = user.tal
         this.role = user.role;
+    }
+
+    private randomId(): number {
+        const randomIdNumber = Math.floor(Math.random() * 10000000)
+        return randomIdNumber
     }
 
     // Model validation:
 
     // POST register Validation Schema:
     private static postRegisterValidationSchema = Joi.object({
-        userId: Joi.forbidden(),
+        id: Joi.forbidden(),
         firstName: Joi.string().required().min(2).max(25),
         lastName: Joi.string().required().min(2).max(25),
-        username: Joi.string().required().min(2).max(25),
+        email: Joi.string().email().min(5).max(30),
         password: Joi.string().required().min(4).max(15),
-        tal: Joi.number().min(1000000000).max(999999999),
-        role: Joi.string()
+        tal: Joi.number().required().min(1000000000).max(999999999),
+        role: Joi.string().required().validate([Role.Admin,Role.Manager,Role.User])
     });
 
     // Validate POST register:
@@ -39,8 +45,6 @@ class UserModel {
         const result = UserModel.postRegisterValidationSchema.validate(this,{abortEarly:false});
         return result.error?.message;
     };
-
-    // i did not validate the login intentionally so as not to give the hackers useful information.
 
 };
 
