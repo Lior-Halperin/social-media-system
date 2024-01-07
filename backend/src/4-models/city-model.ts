142cc45a4cd1b93541db14db72b883df13638778
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2024-01-07 21:03:53 +0200

    Edited method definition to privet, added set/get functions and ICityModel interface.

--- backend/src/4-models/city-model.ts
+++ backend/src/4-models/city-model.ts
@@ -1,14 +1,88 @@
+import Joi from "joi";
+import { ValidationError } from "./errors-model";
 
+interface ICityModel {
+  cityId: number;
+  hebrewName: string;
+  englishName: string;
+}
 class CityModel {
-    private cityId: string;
-    private hebrewName: string;
-    private englishName: string;
+  private _cityId: number;
+  private _hebrewName: string;
+  private _englishName: string;
 
-    public constructor(city: CityModel){
-        this.cityId = city.cityId;
-        this.englishName = city.englishName;
-        this.hebrewName = city.hebrewName
+  public constructor(city: CityModel) {
+    try {
+      this.validation(city)
+      this._cityId = city.cityId;
+      this._englishName = city.englishName;
+      this._hebrewName = city.hebrewName;
+    } catch (err: any) {
+      throw err;
     }
+  }
+
+  public get cityId() {
+    return this._cityId;
+  }
+  public set cityId(id: number) {
+    this.validation('cityId',id)
+    this._cityId = id;
+  }
+
+  public get hebrewName() {
+    return this._hebrewName;
+  }
+
+  public set hebrewName(name: string) {
+    this.validation('hebrewName', name)
+    this._hebrewName = name;
+  }
+
+  public get englishName() {
+    return this._englishName;
+  }
+
+  public set englishName(name: string) {
+    try{
+        this.validation('englishName',name)
+        this._englishName = name;
+    }
+    catch(err:any){
+        throw err
+    }
+  }
+  private validation (property: keyof ICityModel | CityModel, value?: string | number) {
+    try{
+        const error = this.validatePostUploadCity(property,value)
+        if(error){
+            throw new ValidationError(error);
+        }
+    }
+    catch(err:any){
+        throw err
+    }
+  }
+  private static postUploadCityValidationSchema = Joi.object({
+    cityId: Joi.number().required().min(7).max(10000),
+    hebrewName: Joi.string().required().min(2).max(30),
+    englishName: Joi.string().required().min(2).max(30),
+  });
+
+  private validatePostUploadCity(property: keyof ICityModel | CityModel, value?: string | number): string {
+    try {
+        let singleValidation;
+        if (value){
+            singleValidation = {property: value}
+        }
+      const result = CityModel.postUploadCityValidationSchema.validate(singleValidation ? singleValidation : property, {
+        abortEarly: false,
+      });
+      return result.error?.message;
+    } catch (err: any) {
+      throw err;
+    }
+  }
 }
 
-export default CityModel
+export default CityModel;
