e7b50fea3107c1bdb8e57a5b64aced57ac4da1b5
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2024-04-29 22:40:23 +0300

    Added config.ts file for handling environment config.

---
+++ frontend/src/Utils/Config.ts
@@ -0,0 +1,31 @@
+import UrlDomainModel from "../Models/UrlDomainModel"
+
+const urlDomain: UrlDomainModel = new UrlDomainModel('http://localhost:3002') 
+
+export interface IConfig {
+    baseURL:string
+    socialCustomerEndpoint: string
+}
+
+const environments:any = { 
+    development: {
+      baseURL: `${urlDomain.protocol}`+`${urlDomain.domain}`+'api/',
+      socialCustomerEndpoint:'/socialCustomer',
+    },
+    test: {
+        baseURL: `${urlDomain.protocol}`+`${urlDomain.domain}`+'api/',
+        socialCustomerEndpoint:'/socialCustomer',
+    },
+    production: {
+        baseURL: `${urlDomain.protocol}`+`${urlDomain.domain}`+'api/',
+        socialCustomerEndpoint:'/socialCustomer',
+    }
+  }
+  
+  const currentEnvironment = process.env.NODE_ENV || 'development'
+  
+  const config:IConfig = {
+    ...environments[currentEnvironment],
+  }
+  
+  export default config;
