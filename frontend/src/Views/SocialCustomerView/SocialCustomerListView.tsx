c86354f852a11fcf37219c63b8cdfe85c20aae9a
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2024-06-24 14:48:40 +0300

    Edited `HoveringButton` component props.

--- frontend/src/Views/SocialCustomerView/SocialCustomerListView.tsx
+++ frontend/src/Views/SocialCustomerView/SocialCustomerListView.tsx
@@ -8,35 +8,36 @@ import { RootState } from "src/redux/store";
 
 function SocialCustomerListView(): JSX.Element {
   // Utilize the custom hook to access socialCustomers data and functionalities
-  const {
-    socialCustomer,
-    error,
-    isError,
-    isLoading,
-  } = useSocialCustomer();
+  const { socialCustomer, error, isError, isLoading } = useSocialCustomer();
 
   // Select the socialCustomer slice from the Redux store
   const selectedCustomer = useSelector(
-    (state: RootState
-    ) => state.socialCustomer.selectedCustomer
+    (state: RootState) => state.socialCustomer.selectedCustomer
   );
   const dispatch = useDispatch();
 
-  const handleSelectedItemsChange = (newSelectedItems: Record<string, ISocialCustomerModel>) => {
+  const handleSelectedItemsChange = (
+    newSelectedItems: Record<string, ISocialCustomerModel>
+  ) => {
     dispatch(setSelectedCustomer(newSelectedItems));
   };
   if (isLoading) return <div>Loading...</div>;
-    // if (isError) return <div>Error: {error?.message}</div>;
+  // if (isError) return <div>Error: {error?.message}</div>;
 
   return (
     <>
       <GenericTable<ISocialCustomerModel>
-      data={socialCustomer}
-      selectedItems={selectedCustomer}
-      getItemId={(item) => item.customerId}
-      onSelectedItemsChange={handleSelectedItemsChange}
-    />  
-    <HoveringButton selectedCustomers={selectedCustomer} />
+        data={socialCustomer}
+        selectedItems={selectedCustomer}
+        getItemId={(item) => item.customerId}
+        onSelectedItemsChange={handleSelectedItemsChange}
+      />
+      <HoveringButton<ISocialCustomerModel>
+        data={socialCustomer}
+        selectedItems={selectedCustomer}
+        getItemId={(item) => item.customerId}
+        onSelectedItemsChange={handleSelectedItemsChange}
+      />
     </>
   );
 }
