a207dd46a755b20b221c6b3064e0afbe7f5d7963
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2024-07-07 07:53:43 +0300

    Added to `GenericTable` a input animation.

--- frontend/src/Components/GenericTable/GenericTable.tsx
+++ frontend/src/Components/GenericTable/GenericTable.tsx
@@ -3,86 +3,132 @@ import {
   StyledWrapper,
   StyleTableRow,
   StyledTable,
-  StyledTableData,
   StyledTableHeader,
+  StyleFlipTbody,
+  StyleFlipTableRowInner,
+  StyledFlipTableDataFront,
+  StyledFlipTableDataBack,
+  StyledInput,
 } from "./GenericTable.styled";
 import Checkbox from "../Checkbox/Checkbox";
 
-
 export interface GenericTableProps<T extends object> {
-    data: T[];
-    selectedItems: Record<string, T>;
-    getItemId: (item: T) => number;
-    onSelectedItemsChange: (selectedItems: Record<string, T>) => void;
-  }
-  
-  function GenericTable<T extends object>({
-    data,
-    selectedItems,
-    getItemId,
-    onSelectedItemsChange,
-  }: GenericTableProps<T>): JSX.Element {
-  
-    const [tableData, setTableData] = useState<T[]>([]);
-  
-    useEffect(() => {
-      setTableData(data);
-    }, [data]);
-  
-    const handleCheckboxChange = useCallback(
-      (item: T) => {
-        const newCheckboxState = { ...selectedItems };
-        const itemId = getItemId(item);
-        if (newCheckboxState[itemId]) {
-          delete newCheckboxState[itemId];
-        } else {
-          newCheckboxState[itemId] = item;
-        }
-        onSelectedItemsChange(newCheckboxState);
-        return newCheckboxState;
+  data: T[];
+  selectedItems: Record<string, T>;
+  getItemId: (item: T) => number;
+  onSelectedItemsChange: (selectedItems: Record<string, T>) => void;
+}
+
+function GenericTable<T extends object>({
+  data,
+  selectedItems,
+  getItemId,
+  onSelectedItemsChange,
+}: GenericTableProps<T>): JSX.Element {
+  const [tableData, setTableData] = useState<T[]>([]);
+  const [flippedRow, setFlippedRow] = useState<number | null>(null);
+  const [editedData, setEditedData] = useState<Record<number, Partial<T>>>({});
+
+  useEffect(() => {
+    setTableData(data);
+  }, [data]);
+
+  const handleCardClick = (itemId: number) => {
+    setFlippedRow(flippedRow === itemId ? null : itemId);
+  };
+
+  const handleInputChange = (itemId: number, field: keyof T, value: string) => {
+    setEditedData((prevData) => ({
+      ...prevData,
+      [itemId]: {
+        ...prevData[itemId],
+        [field]: value,
       },
-      [selectedItems, getItemId, onSelectedItemsChange]
-    );
-  
-    const tableHeaders = tableData.length > 0 ? Object.keys(tableData[0]) : [];
-  
-    if (tableData.length === 0) {
-      return <StyledWrapper key={Math.random()}>Loading...</StyledWrapper>;
-    }
-  
-    return (
-      <StyledWrapper className="generic-table" key={Math.random()}>
-        <StyledTable>
-          <thead>
-            <StyleTableRow>
-              <StyledTableHeader key={"header"}>{""}</StyledTableHeader>
-              {tableHeaders.map((header) => (
-                <StyledTableHeader key={header}>{header}</StyledTableHeader>
-              ))}
-            </StyleTableRow>
-          </thead>
-          <tbody>
-            {tableData.map((item) => (
-              <StyleTableRow key={getItemId(item)}>
+    }));
+  };
+
+  const handleCheckboxChange = useCallback(
+    (item: T) => {
+      const newCheckboxState = { ...selectedItems };
+      const itemId = getItemId(item);
+      if (newCheckboxState[itemId]) {
+        delete newCheckboxState[itemId];
+      } else {
+        newCheckboxState[itemId] = item;
+      }
+      onSelectedItemsChange(newCheckboxState);
+      return newCheckboxState;
+    },
+    [selectedItems, getItemId, onSelectedItemsChange]
+  );
+
+  const tableHeaders = tableData.length > 0 ? Object.keys(tableData[0]) : [];
+
+  if (tableData.length === 0) {
+    return <StyledWrapper key={Math.random()}>Loading...</StyledWrapper>;
+  }
+
+  return (
+    <StyledWrapper className="generic-table" key={Math.random()}>
+      <StyledTable>
+        <thead>
+          <StyleTableRow>
+            <StyledTableHeader key={"header"}>{""}</StyledTableHeader>
+            {tableHeaders.map((header) => (
+              <StyledTableHeader key={header}>{header}</StyledTableHeader>
+            ))}
+          </StyleTableRow>
+        </thead>
+        <StyleFlipTbody>
+          {tableData.map((item) => {
+            const itemId = getItemId(item);
+            const isFlipped = flippedRow === itemId;
+            return (
+              <StyleFlipTableRowInner
+                key={itemId}
+                onClick={() => handleCardClick(itemId)}
+              >
                 <td>
                   <Checkbox
-                    id={`checkbox-${getItemId(item)}`}
+                    id={`checkbox-${itemId}`}
                     label=""
-                    checked={!!selectedItems[getItemId(item)]}
+                    checked={!!selectedItems[itemId]}
                     onChange={() => handleCheckboxChange(item)}
                   />
                 </td>
-                {tableHeaders.map((header) => (
-                  <StyledTableData key={`${getItemId(item)}-${header}`}>
-                    {String(item[header as keyof T])}
-                  </StyledTableData>
-                ))}
-              </StyleTableRow>
-            ))}
-          </tbody>
-        </StyledTable>
-      </StyledWrapper>
-    );
-  }
-  
-  export default GenericTable;
+                {tableHeaders.map((header) =>
+                  isFlipped ? (
+                    <StyledFlipTableDataBack
+                      $isflipped={isFlipped} // Use $ prefix for transient props
+                      key={`${itemId}-${header}`}
+                    >
+                      <StyledInput
+                        type="text"
+                        value={String(item[header as keyof T])}
+                        onChange={(e) =>
+                          handleInputChange(
+                            itemId,
+                            header as keyof T,
+                            e.target.value
+                          )
+                        }
+                      />
+                    </StyledFlipTableDataBack>
+                  ) : (
+                    <StyledFlipTableDataFront
+                      key={`${itemId}-${header}`}
+                    >
+                      {String(item[header as keyof T])}
+                    </StyledFlipTableDataFront>
+                  )
+                )}
+              </StyleFlipTableRowInner>
+            );
+          })}
+        </StyleFlipTbody>
+      </StyledTable>
+    </StyledWrapper>
+  );
+}
+
+export default GenericTable;
