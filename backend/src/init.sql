a89f6a2da5275564ae1b8dfc9f7349fde1124899
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2024-09-01 11:50:18 +0300

    Edited init.sql file.

--- backend/src/init.sql
+++ backend/src/init.sql
@@ -11,7 +11,7 @@ CREATE TABLE
         last_name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL,
-        tal INT,
+        tel INT,
         role ENUM ('User', 'Manager', 'Admin') DEFAULT 'User'
     );
 
@@ -19,35 +19,38 @@ CREATE TABLE
     IF NOT EXISTS social_customers (
         customer_id INT PRIMARY KEY AUTO_INCREMENT,
         first_name VARCHAR(30) NOT NULL,
-        last_name VARCHAR(30) NOT NULL,
-        update_date DATE NOT NULL,         
+        last_name VARCHAR(30) NOT NULL
     );
 
 CREATE TABLE
     IF NOT EXISTS addresses (
         address_id INT PRIMARY KEY AUTO_INCREMENT,
         customer_id INT NOT NULL,
-        country: string DEFAULT israel,
-        city: VARCHAR(30) NOT NULL,
+        country VARCHAR(30) DEFAULT "israel",
+        city VARCHAR(30) NOT NULL,
         street VARCHAR(30) NOT NULL,
         house_number VARCHAR(4) NOT NULL,
         apartment_number INT,
         floor INT NOT NULL,
-        longitude INT NOT NULL,
-        latitude INT NOT NULL,
-        distance_km_from_jerusalem INT,
-        update_date DATE NOT NULL, 
-        comments VARCHAR(100),        
+        longitude DECIMAL(9, 6) NOT NULL,
+        latitude DECIMAL(9, 6) NOT NULL,
+        distance_km_from_intentional_point INT,
+        update_date DATE NOT NULL,
+        comments VARCHAR(100),
         FOREIGN KEY (customer_id) REFERENCES social_customers (customer_id) ON DELETE CASCADE,
         INDEX (customer_id)
     );
 
 CREATE TABLE
-    IF NOT EXISTS phone (
-        phone_id: INT PRIMARY KEY AUTO_INCREMENT,
-        customer_id: INT NOT NULL,
-        phone_number: INT NOT NULL,
-        INDEX (customer_id)
+    IF NOT EXISTS tel (
+        tel_id INT PRIMARY KEY AUTO_INCREMENT,
+        customer_id INT NOT NULL,
+        tel_number INT NOT NULL,
+        update_date TIMESTAMP NOT NULL,
+        /* The 'TIMESTAMP' type stores date and time. the time zone support and can be automatically updated to the current date and time  */
+        INDEX (customer_id),
+        FOREIGN KEY (customer_id) REFERENCES social_customers (customer_id) ON DELETE CASCADE,
+        INDEX (tel_number)
     );
 
 CREATE TABLE
